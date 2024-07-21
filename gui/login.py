13b449c09d33344601e2defdf356614cccfacacb
Austin <Austin@austinatts.co.uk>
2024-07-21 22:37:26 +0100

    Updated login to add looks

--- gui/login.py
+++ gui/login.py
@@ -1,6 +1,6 @@
 import customtkinter as ctk
+import CTkMessagebox as ctkm
 from gui.main import Main
-
 
 class Login:
     def __init__(self, app):
@@ -9,24 +9,36 @@ class Login:
         self.main = Main(self)
 
     def load_page(self, frame):
-
         def submit():
             self.submit(frame)
 
-        self.title_label = ctk.CTkLabel(frame, text="Login")
+        self.title_label = ctk.CTkLabel(frame, text="Login", font=ctk.CTkFont(size=20, weight="bold"))
+        self.username_label = ctk.CTkLabel(frame, text="Username", anchor="w")
+        self.password_label = ctk.CTkLabel(frame, text="Password", anchor="w")
 
-        self.title_label.grid(row=0, column=0, padx=20, pady=10)
-
-        self.test_textbox = ctk.CTkTextbox(frame)
-        self.test_textbox.grid(row=1, column=0)
+        self.username_entry = ctk.CTkEntry(frame, width=230, height=50)
+        self.password_entry = ctk.CTkEntry(frame, width=230, height=50)
 
         self.submit_button = ctk.CTkButton(frame, text="Submit", command=submit)
-        self.submit_button.grid(row=2, column=0)
+
+        self.username_entry.grid(row=2, column=0, padx=60, pady=10, sticky="nsew")
+        self.password_entry.grid(row=4, column=0, padx=60, pady=10, sticky="nsew")
+
+        self.submit_button.grid(row=5, column=0, padx=60, pady=(0, 20))
+
+        self.title_label.grid(row=0, column=0, padx=60, pady=(20, 10))
+        self.username_label.grid(row=1, column=0, padx=60, pady=(10, 0))
+        self.password_label.grid(row=3, column=0, padx=60, pady=(10, 0))
 
     def submit(self, frame):
-        self.value = self.test_textbox.get("1.0", "end-1c")
-        print(self.value)
-        if self.value == "2":
-            self.main.load_page(frame)
-        else:
-            print("Fuk of")
+        self.clear_page()
+        self.app.geometry(f"{1000}x{580}")
+        self.main.load_page(frame)
+
+    def clear_page(self):
+        self.title_label.destroy()
+        self.username_label.destroy()
+        self.password_label.destroy()
+        self.username_entry.destroy()
+        self.password_entry.destroy()
+        self.submit_button.destroy()
