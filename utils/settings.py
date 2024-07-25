ebe750c23916f12ddb91b63404f7896c53b82153
Austin <Austin@austinatts.co.uk>
2024-07-25 19:32:38 +0100

    Added scale settings

--- utils/settings.py
+++ utils/settings.py
@@ -8,7 +8,12 @@ def colour_scheme(app):
 
 
 def scale():
-    pass
+    scale_input_dialog = ctk.CTkInputDialog(text="Enter a value between 50% and 200%:", title="Scale")
+    scale = scale_input_dialog.get_input()
+    scale_float = float(scale) / 100
+    ctk.set_widget_scaling(scale_float)
+    ctk.set_window_scaling(scale_float)
+
 
 def logout(app):
     app.perform_logout()
