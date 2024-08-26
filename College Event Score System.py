894fc797a49d26520e6001fe1963c04731dd03a6
AustinWelsh-Graham <Austin@austinatts.co.uk>
2024-08-26 12:13:43 +0100

    Removed edit functions as not needed

--- College Event Score System.py
+++ College Event Score System.py
@@ -3,7 +3,6 @@ import CTkMenuBar as ctkmb
 from gui.login import Login
 import CTkMessagebox as ctkm
 from utils.file import save, restore_latest, restore_custom
-from utils.edit import add_record, update_record, delete_record
 from utils.settings import colour_scheme, scale, logout
 from utils.about import website, github, description
 
@@ -25,7 +24,6 @@ class App(ctk.CTk):
         self.title_menu = ctkmb.CTkTitleMenu(self)
 
         self.file_button = self.title_menu.add_cascade("File", fg_color="transparent", hover_color="#CCCCCC")
-        self.edit_button = self.title_menu.add_cascade("Edit", fg_color="transparent", hover_color="#CCCCCC")
         self.settings_button = self.title_menu.add_cascade("Settings", fg_color="transparent", hover_color="#CCCCCC")
         self.about_button = self.title_menu.add_cascade("About", fg_color="transparent", hover_color="#CCCCCC")
 
@@ -35,11 +33,6 @@ class App(ctk.CTk):
         self.file_sub_menu = self.file_dropdown.add_submenu("Open")
         self.file_sub_menu.add_option(option="Restore Latest", command=restore_latest)
         self.file_sub_menu.add_option(option="Restore Custom", command=restore_custom)
-
-        self.edit_dropdown = ctkmb.CustomDropdownMenu(widget=self.edit_button)
-        self.edit_dropdown.add_option(option="Add Record", command=add_record)
-        self.edit_dropdown.add_option(option="Update Record", command=update_record)
-        self.edit_dropdown.add_option(option="Delete Record", command=delete_record)
 
         self.settings_dropdown = ctkmb.CustomDropdownMenu(widget=self.settings_button)
         self.settings_dropdown.add_option(option="Colour Scheme", command=lambda: colour_scheme(self))
